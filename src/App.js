e0f00ca026b93e4924329cd8c31f963a27b76708
dosjung <dosjung@gmail.com>
2020-01-19 21:13:36 +0900

    #6 2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 import "./App.css";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" exact={true} component={About} />
+      <Route path="/about" component={About} />
     </HashRouter>
   );
 }
