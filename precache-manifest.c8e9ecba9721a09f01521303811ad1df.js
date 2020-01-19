789d6ff939dd1123cc9a850dd10e44c82c63bfd4
dosjung <dosjung@gmail.com>
2020-01-19 14:48:44 +0900

    Updates

---
+++ precache-manifest.c8e9ecba9721a09f01521303811ad1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01533cab30f0ca0cda6c1045f4ebd3f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "757263e7b7a3d70d0676",
+    "url": "/movie_app_2019/static/css/main.8b72877d.chunk.css"
+  },
+  {
+    "revision": "2285b3cdfe7d06f12a50",
+    "url": "/movie_app_2019/static/js/2.53a1a87c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.53a1a87c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "757263e7b7a3d70d0676",
+    "url": "/movie_app_2019/static/js/main.5c269433.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
