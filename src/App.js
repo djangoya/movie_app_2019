153d106dfe2ed24363339507187b17c95c708d3f
dosjung <dosjung@gmail.com>
2020-01-19 21:38:43 +0900

    #6 3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 import { HashRouter, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
